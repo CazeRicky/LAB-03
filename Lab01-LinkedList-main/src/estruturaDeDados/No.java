bbb9529335b4594833ca3594e3e970ab07fc12f0
Carlos Henrique de Oliveira Rodrigues <100224655+CazeRicky@users.noreply.github.com>
2024-12-03 18:11:14 -0300

    Update No.java

--- Lab01-LinkedList-main/src/estruturaDeDados/No.java
+++ Lab01-LinkedList-main/src/estruturaDeDados/No.java
@@ -1,11 +1,11 @@
 package estruturaDeDados;
 
 public class No {
-    int value;      // Valor armazenado no no
-    No next;        // Referencia para o prox
+    int valor;
+    No proximo;
 
-    public No(int value) {
-        this.value = value;
-        this.next = null;
+    public No(int valor) {
+        this.valor = valor;
+        this.proximo = null;
     }
 }
